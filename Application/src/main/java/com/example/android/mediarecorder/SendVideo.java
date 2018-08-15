44196e09aa3d3f4e1f8f87e80c50db09d2183be4
emiliedao <edao@live.fr>
2018-08-15 16:07:16 +1200

    android media recorder

---
+++ Application/src/main/java/com/example/android/mediarecorder/SendVideo.java
@@ -0,0 +1,61 @@
+package com.example.android.mediarecorder;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Toast;
+
+public class SendVideo extends AppCompatActivity {
+
+    private String mVideoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_video);
+
+        // Bundle to get the parameters passed to the activity
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            mVideoPath = extras.getString("path");
+        }
+    }
+
+    public void onSendClick(View view) {
+        uploadVideo();
+    }
+
+    private void uploadVideo() {
+        class UploadVideo extends AsyncTask<Void, Void, String> {
+
+            ProgressDialog uploading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                uploading = ProgressDialog.show(SendVideo.this, "Uploading File", "Please wait...", false, false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                uploading.dismiss();
+                Toast toast = Toast.makeText(getApplicationContext(),  s, Toast.LENGTH_LONG);
+                toast.show();
+                finish();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                Upload u = new Upload();
+                String msg = u.uploadVideo(mVideoPath);
+                return msg;
+            }
+        }
+        UploadVideo uv = new UploadVideo();
+        uv.execute();
+    }
+}
